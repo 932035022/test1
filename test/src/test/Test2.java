03b6aca261fd7c36876ff59b6841791024aac240
000 <000@000-PC>
2020-01-03 11:32:29 +0800

    test for

---
+++ test/src/test/Test2.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test2 {
+	public static void main(String[] args) {
+		int a = 10;
+		for(int i=0;i<a;i++) {
+			int b = i++;
+			System.out.println(b);
+		}
+		
+	}
+}
